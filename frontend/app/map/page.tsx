19cc9d035645b6cc6aadca9960bbf08d371adef0
ishan22399 <ishanshivankar14@gmail.com>
2026-01-16 01:51:14 +0530

    Add map page

---
+++ frontend/app/map/page.tsx
@@ -0,0 +1,8 @@
+export default function MapPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Global Map Page</h1>
+      <p>Content for Global Map will go here.</p>
+    </div>
+  );
+}
