1398748001091b321529dedaf8e8778157fd2a6f
ishan22399 <ishanshivankar14@gmail.com>
2026-01-16 01:51:19 +0530

    Add news page

---
+++ frontend/app/news/page.tsx
@@ -0,0 +1,8 @@
+export default function NewsPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>News & Alerts Page</h1>
+      <p>Content for News & Alerts will go here.</p>
+    </div>
+  );
+}
